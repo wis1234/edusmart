538a8114c0f1348ea0a8db5452fdcab6ff44f6b4
wis1234 <ronaldoagbohou@gmail.com>
2025-07-01 11:39:22 +0100

    fixing call type 3( server TURN)

--- public/js/video-call.js
+++ public/js/video-call.js
@@ -5,7 +5,12 @@ const rtcConfig = {
         { urls: 'stun:stun1.l.google.com:19302' },
         { urls: 'stun:stun2.l.google.com:19302' },
         { urls: 'stun:stun3.l.google.com:19302' },
-        { urls: 'stun:stun4.l.google.com:19302' }
+        { urls: 'stun:stun4.l.google.com:19302' },
+        {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        }
     ],
     iceCandidatePoolSize: 10,
     bundlePolicy: 'max-bundle',
